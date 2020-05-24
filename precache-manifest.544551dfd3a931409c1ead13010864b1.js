9a7ec0061b07106e358aa7184491ad4193b03fa2
SergeLaskovski <lightgrey1238@gmail.com>
2020-05-24 17:35:53 +1200

    Updates

---
+++ precache-manifest.544551dfd3a931409c1ead13010864b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2fc5be37d39fd51b69e8affaf716ce",
+    "url": "/flexhire/index.html"
+  },
+  {
+    "revision": "fd765c352bcb2799793f",
+    "url": "/flexhire/static/css/2.3bf2db42.chunk.css"
+  },
+  {
+    "revision": "dac22dc10b9c532924da",
+    "url": "/flexhire/static/css/main.81ae3604.chunk.css"
+  },
+  {
+    "revision": "fd765c352bcb2799793f",
+    "url": "/flexhire/static/js/2.9f14a8c5.chunk.js"
+  },
+  {
+    "revision": "ab07a9a742436dc2c5f2d0004e0cebbf",
+    "url": "/flexhire/static/js/2.9f14a8c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac22dc10b9c532924da",
+    "url": "/flexhire/static/js/main.b5805cb9.chunk.js"
+  },
+  {
+    "revision": "deb8816d782be3f14b2c",
+    "url": "/flexhire/static/js/runtime-main.d190697e.js"
+  },
+  {
+    "revision": "e8c342df5ef71cb5319ccd7e69e737fe",
+    "url": "/flexhire/static/media/headerLogo.e8c342df.png"
+  }
+]);
